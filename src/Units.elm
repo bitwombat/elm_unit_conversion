561919b851170eea96e5877d499fcf798b158c0d
Greg Bell <greg@bitwombat.com.au>
2021-09-15 19:14:37 +1000

    Basic algorithm operating!

--- src/Units.elm
+++ src/Units.elm
@@ -1,51 +1,97 @@
-module Units exposing (main)
+module Units exposing (..)
 
-import Html exposing (Html, div, h1, hr, img, input, text)
-import Html.Attributes exposing (..)
-
-
-type NumberWithUnit
-    = Empty
-    | Value Float String Float String
+import Dict exposing (..)
 
 
-x : NumberWithUnit
-x =
-    Value 5.0 "J" 1.0 "s"
+type Quantity
+    = Quantity Float String
 
 
-y : NumberWithUnit
-y =
-    Value 1.6 "km" 1 "mile"
-
-
-chain : List NumberWithUnit
-chain =
-    [ x, y ]
+type Term
+    = Term Quantity Quantity
 
 
 
---chainView : List NumberWithUnit -> Html msg
---chainView chain =
+-- = 0.5 J/s   500 GWh/year = ____ J/s
+-- `
+-- 5 m/s^2 = 5 = m/s/s = 5 (m/s)/s = 5 m   1 = 5 m    1   1
+--                         - x -       x  - x -
+--                         s   s          s   s
+--
+-- Just use tuples?
+-- Lists? m/s/s
 
 
-unitView : Html msg
-unitView =
-    div [ class "unit" ]
-        [ input [ class "numerator" ] []
-        , hr [] []
-        , input [ class "denominator" ] []
-        ]
+combine : Term -> Term -> Term
+combine (Term (Quantity fn1 sn1) (Quantity fd1 sd1)) (Term (Quantity fn2 sn2) (Quantity fd2 sd2)) =
+    Term (Quantity (fn1 * fn2) (sn1 ++ sn2)) (Quantity (fd1 * fd2) (sd1 ++ sd2))
 
 
-view c =
-    div
-        [ class "content" ]
-        [ h1 [] [ text "Magical Unit Converter (MUC)" ]
-        , div [ id "expression" ]
-            [ unitView, text "x", unitView, text "=", unitView ]
-        ]
+inserts : Char -> Dict Char Int -> Dict Char Int
+inserts c d =
+    case Dict.get c d of
+        Just v ->
+            Dict.insert c (v + 1) d
+
+        Nothing ->
+            Dict.insert c 1 d
 
 
-main =
-    view chain
+countUnits : String -> Dict Char Int
+countUnits s =
+    String.foldl inserts Dict.empty s
+
+
+onlyLeft : Char -> Int -> Dict Char Int -> Dict Char Int
+onlyLeft k v accum =
+    Dict.insert k v accum
+
+
+onlyRight : Char -> Int -> Dict Char Int -> Dict Char Int
+onlyRight k v accum =
+    Dict.insert k (-1 * v) accum
+
+
+inBoth : Char -> Int -> Int -> Dict Char Int -> Dict Char Int
+inBoth k v1 v2 accum =
+    Dict.insert k (v1 - v2) accum
+
+
+reallyCancel : Dict Char Int -> Dict Char Int -> Dict Char Int
+reallyCancel n d =
+    Dict.merge onlyLeft inBoth onlyRight n d Dict.empty
+
+
+cancelUnits : String -> String -> Dict Char Int
+cancelUnits n d =
+    reallyCancel (countUnits n) (countUnits d)
+
+
+splitTest : Char -> Int -> Bool
+splitTest k v =
+    v > 0
+
+
+valueIsNonZero : Char -> Int -> Bool
+valueIsNonZero _ v =
+    v /= 0
+
+
+filterOutOnes : Dict Char Int -> Dict Char Int
+filterOutOnes d =
+    Dict.filter valueIsNonZero d
+
+
+fixSign : Char -> Int -> Int
+fixSign _ v =
+    -1 * v
+
+
+fixSignDeux : ( Dict Char Int, Dict Char Int ) -> ( Dict Char Int, Dict Char Int )
+fixSignDeux ( n, d ) =
+    ( n, Dict.map fixSign d )
+
+
+cancelUnitsDeux : String -> String -> ( Dict Char Int, Dict Char Int )
+cancelUnitsDeux n d =
+    fixSignDeux <| Dict.partition splitTest (filterOutOnes <| cancelUnits n d)
---
+++ tests/Tests.elm
@@ -0,0 +1,92 @@
+module Tests exposing (tests)
+
+import Dict exposing (fromList)
+import Expect
+import Test exposing (..)
+import Units exposing (..)
+
+
+t1 : Term
+t1 =
+    Term (Quantity 1.0 "J") (Quantity 2.0 "s")
+
+
+t2 : Term
+t2 =
+    Term (Quantity 1.0 "m") (Quantity 1.0 "m")
+
+
+t3 : Term
+t3 =
+    Term (Quantity 1.0 "F") (Quantity 1.0 "F")
+
+
+tests : Test
+tests =
+    describe "Make units"
+        [ test "Make unit" <|
+            \() ->
+                Expect.equal t1
+                    (Term (Quantity 1.0 "J") (Quantity 2.0 "s"))
+        , test "Test combining" <|
+            \() ->
+                Expect.equal (Term (Quantity 1.0 "Jm") (Quantity 2.0 "sm"))
+                    (combine t1 t2)
+        , test "Test 3-way combining" <|
+            \() ->
+                Expect.equal (Term (Quantity 1.0 "JmF") (Quantity 2.0 "smF"))
+                    (combine
+                        t2
+                        t3
+                        |> combine t1
+                    )
+        , test "Test 3-way combining with f.g composition (exercise)" <|
+            \() ->
+                Expect.equal (Term (Quantity 1.0 "JmF") (Quantity 2.0 "smF"))
+                    ((combine t1 << combine t2)
+                        t3
+                    )
+        , test "Test 3-way combining with g.f composition (exercise)" <|
+            \() ->
+                Expect.equal (Term (Quantity 1.0 "JmF") (Quantity 2.0 "smF"))
+                    ((combine t2 >> combine t1)
+                        t3
+                    )
+        , test "Test 3-way combining with pipelines (exercise)" <|
+            \() ->
+                Expect.equal (Term (Quantity 1.0 "JmF") (Quantity 2.0 "smF"))
+                    (t3 |> combine t2 |> combine t1)
+        , test "Test unit counting" <|
+            \() ->
+                Expect.equal
+                    (Dict.fromList
+                        [ ( 'a', 1 )
+                        , ( 'b', 2 )
+                        , ( 'c', 3 )
+                        ]
+                    )
+                    (countUnits "abbccc")
+        , test "Test unit cancelling" <|
+            \() ->
+                Expect.equal
+                    (Dict.fromList
+                        [ ( 'a', 0 )
+                        , ( 'b', 1 )
+                        , ( 'c', 2 )
+                        , ( 'd', -1 )
+                        ]
+                    )
+                    (cancelUnits "abbccc" "abcd")
+        , test "Test unit cancelling, part deux" <|
+            \() ->
+                Expect.equal
+                    ( Dict.fromList
+                        [ ( 'b', 1 )
+                        , ( 'c', 2 )
+                        ]
+                    , Dict.fromList
+                        [ ( 'd', 1 )
+                        ]
+                    )
+                    (cancelUnitsDeux "abbccc" "abcd")
+        ]
