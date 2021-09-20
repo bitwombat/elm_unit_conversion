8116e57bc67f99fbe264ae4de2de422a9eb169f2
Greg Bell <greg@bitwombat.com.au>
2021-09-21 07:49:14 +1000

    Tests passing

--- src/Units.elm
+++ src/Units.elm
@@ -1,34 +1,60 @@
 module Units exposing (..)
 
+{- TODO:
+
+   Support 5 J/s
+             ---
+             km
+
+   Split km into 1000 m so that k's can cancel k's
+
+-}
+
 import Dict exposing (..)
+import List
+import String
 
 
 type Quantity
-    = Quantity Float String
+    = Quantity Float (List String)
 
 
 type Term
     = Term Quantity Quantity
 
 
-
--- = 0.5 J/s   500 GWh/year = ____ J/s
--- `
--- 5 m/s^2 = 5 = m/s/s = 5 (m/s)/s = 5 m   1 = 5 m    1   1
---                         - x -       x  - x -
---                         s   s          s   s
---
--- Just use tuples?
--- Lists? m/s/s
+unity : Quantity
+unity =
+    Quantity 1.0 []
 
 
-combine : Term -> Term -> Term
-combine (Term (Quantity fn1 sn1) (Quantity fd1 sd1)) (Term (Quantity fn2 sn2) (Quantity fd2 sd2)) =
+unityFraction : Term
+unityFraction =
+    Term unity unity
+
+
+multiply : Term -> Term -> Term
+multiply (Term (Quantity fn1 sn1) (Quantity fd1 sd1)) (Term (Quantity fn2 sn2) (Quantity fd2 sd2)) =
     Term (Quantity (fn1 * fn2) (sn1 ++ sn2)) (Quantity (fd1 * fd2) (sd1 ++ sd2))
 
 
-inserts : Char -> Dict Char Int -> Dict Char Int
-inserts c d =
+cancel : Term -> Term
+cancel (Term (Quantity fn1 sn1) (Quantity fd1 sd1)) =
+    let
+        ( numer, denom ) =
+            cancelUnits sn1 sd1
+    in
+    Term (Quantity (fn1 / fd1) numer) (Quantity 1.0 denom)
+
+
+multiplyAndCancel : Term -> Term -> Term
+multiplyAndCancel t1 t2 =
+    multiply t1 t2
+        |> cancel
+
+
+countUnitOccurrences : String -> Dict String Int -> Dict String Int
+countUnitOccurrences c d =
     case Dict.get c d of
         Just v ->
             Dict.insert c (v + 1) d
@@ -37,61 +63,131 @@ inserts c d =
             Dict.insert c 1 d
 
 
-countUnits : String -> Dict Char Int
-countUnits s =
-    String.foldl inserts Dict.empty s
+unitListToDict : List String -> Dict String Int
+unitListToDict s =
+    List.foldl countUnitOccurrences Dict.empty s
 
 
-onlyLeft : Char -> Int -> Dict Char Int -> Dict Char Int
-onlyLeft k v accum =
+unitInNumer : String -> Int -> Dict String Int -> Dict String Int
+unitInNumer k v accum =
     Dict.insert k v accum
 
 
-onlyRight : Char -> Int -> Dict Char Int -> Dict Char Int
-onlyRight k v accum =
+unitInDenom : String -> Int -> Dict String Int -> Dict String Int
+unitInDenom k v accum =
     Dict.insert k (-1 * v) accum
 
 
-inBoth : Char -> Int -> Int -> Dict Char Int -> Dict Char Int
-inBoth k v1 v2 accum =
+unitInBoth : String -> Int -> Int -> Dict String Int -> Dict String Int
+unitInBoth k v1 v2 accum =
     Dict.insert k (v1 - v2) accum
 
 
-reallyCancel : Dict Char Int -> Dict Char Int -> Dict Char Int
-reallyCancel n d =
-    Dict.merge onlyLeft inBoth onlyRight n d Dict.empty
+combineUnitNumerAndDenom : List String -> List String -> Dict String Int
+combineUnitNumerAndDenom ns ds =
+    let
+        n =
+            unitListToDict ns
+
+        d =
+            unitListToDict ds
+    in
+    Dict.merge unitInNumer unitInBoth unitInDenom n d Dict.empty
 
 
-cancelUnits : String -> String -> Dict Char Int
-cancelUnits n d =
-    reallyCancel (countUnits n) (countUnits d)
-
-
-splitTest : Char -> Int -> Bool
-splitTest _ v =
-    v > 0
-
-
-valueIsNonZero : Char -> Int -> Bool
+valueIsNonZero : String -> Int -> Bool
 valueIsNonZero _ v =
     v /= 0
 
 
-filterOutOnes : Dict Char Int -> Dict Char Int
+{-| If the exponent is 0, that's the same as 1, and not needed in unit
+conversion multiplications.
+-}
+filterOutOnes : Dict String Int -> Dict String Int
 filterOutOnes d =
     Dict.filter valueIsNonZero d
 
 
-fixSign : Char -> Int -> Int
-fixSign _ v =
-    -1 * v
+isPositive : String -> Int -> Bool
+isPositive _ v =
+    v > 0
 
 
-fixSignDeux : ( Dict Char Int, Dict Char Int ) -> ( Dict Char Int, Dict Char Int )
-fixSignDeux ( n, d ) =
-    ( n, Dict.map fixSign d )
+splitIntoNumerAndDenom : Dict String Int -> ( Dict String Int, Dict String Int )
+splitIntoNumerAndDenom =
+    Dict.partition isPositive
 
 
-cancelUnitsDeux : String -> String -> ( Dict Char Int, Dict Char Int )
-cancelUnitsDeux n d =
-    fixSignDeux <| Dict.partition splitTest (filterOutOnes <| cancelUnits n d)
+{-| The denominator has negative numbers (that was the result of the combining,
+and left in place for the splitting)
+Give the denominator the correct exponents by negating all of them.
+-}
+fixDenomSign : ( Dict String Int, Dict String Int ) -> ( Dict String Int, Dict String Int )
+fixDenomSign ( n, d ) =
+    ( n, Dict.map (\_ v -> negate v) d )
+
+
+toStr : String -> Int -> List String -> List String
+toStr unit exponent ss =
+    List.append ss (List.repeat exponent unit)
+
+
+dictToList : Dict String Int -> List String
+dictToList t =
+    foldl toStr [] t
+
+
+cancelUnits : List String -> List String -> ( List String, List String )
+cancelUnits n d =
+    let
+        ( nDict, dDict ) =
+            combineUnitNumerAndDenom n d
+                |> filterOutOnes
+                |> splitIntoNumerAndDenom
+                |> fixDenomSign
+    in
+    ( dictToList nDict, dictToList dDict )
+
+
+toDisplayStr : String -> Int -> List String -> List String
+toDisplayStr unit exponent ss =
+    case exponent of
+        1 ->
+            List.append ss [ unit ]
+
+        _ ->
+            List.append ss [ unit ++ "<sup>" ++ String.fromInt exponent ++ "</sup>" ]
+
+
+dictToDisplayList : Dict String Int -> List String
+dictToDisplayList t =
+    foldl toDisplayStr [] t
+
+
+multiplyList : List Term -> Term
+multiplyList ts =
+    List.foldl multiplyAndCancel unityFraction ts
+
+
+concatWithDot : String -> String -> String
+concatWithDot new existing =
+    if existing == "" then
+        new
+
+    else
+        new ++ "∙" ++ existing
+
+
+listToString : List String -> String
+listToString ss =
+    List.foldr concatWithDot "" ss
+
+
+dictFractToDisplayString : ( Dict String Int, Dict String Int ) -> ( String, String )
+dictFractToDisplayString ( n, d ) =
+    ( listToString (dictToDisplayList n), listToString (dictToDisplayList d) )
+
+
+
+-- TODO: foldl gives the backwards result "cba" instead of "abc". Why? Turn the
+-- foldls to foldrs throughout?
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -8,17 +8,17 @@ import Units exposing (..)
 
 t1 : Term
 t1 =
-    Term (Quantity 1.0 "J") (Quantity 2.0 "s")
+    Term (Quantity 1.0 [ "J" ]) (Quantity 2.0 [ "s" ])
 
 
 t2 : Term
 t2 =
-    Term (Quantity 1.0 "m") (Quantity 1.0 "m")
+    Term (Quantity 1.0 [ "m" ]) (Quantity 1.0 [ "m" ])
 
 
 t3 : Term
 t3 =
-    Term (Quantity 1.0 "F") (Quantity 1.0 "F")
+    Term (Quantity 1.0 [ "F" ]) (Quantity 1.0 [ "F" ])
 
 
 tests : Test
@@ -27,66 +27,124 @@ tests =
         [ test "Make unit" <|
             \() ->
                 Expect.equal t1
-                    (Term (Quantity 1.0 "J") (Quantity 2.0 "s"))
-        , test "Test combining" <|
+                    (Term (Quantity 1.0 [ "J" ]) (Quantity 2.0 [ "s" ]))
+        , test "Test multiplication" <|
             \() ->
-                Expect.equal (Term (Quantity 1.0 "Jm") (Quantity 2.0 "sm"))
-                    (combine t1 t2)
-        , test "Test 3-way combining" <|
+                Expect.equal (Term (Quantity 1.0 [ "J", "m" ]) (Quantity 2.0 [ "s", "m" ]))
+                    (multiply t1 t2)
+        , test "Test 3-way multiplication" <|
             \() ->
-                Expect.equal (Term (Quantity 1.0 "JmF") (Quantity 2.0 "smF"))
-                    (combine
+                Expect.equal (Term (Quantity 1.0 [ "J", "m", "F" ]) (Quantity 2.0 [ "s", "m", "F" ]))
+                    (multiply
                         t2
                         t3
-                        |> combine t1
+                        |> multiply t1
                     )
-        , test "Test 3-way combining with f.g composition (exercise)" <|
+        , test "Test 3-way multiplication with f.g composition (exercise)" <|
             \() ->
-                Expect.equal (Term (Quantity 1.0 "JmF") (Quantity 2.0 "smF"))
-                    ((combine t1 << combine t2)
+                Expect.equal (Term (Quantity 1.0 [ "J", "m", "F" ]) (Quantity 2.0 [ "s", "m", "F" ]))
+                    ((multiply t1 << multiply t2)
                         t3
                     )
-        , test "Test 3-way combining with g.f composition (exercise)" <|
+        , test "Test 3-way multiplication with g.f composition (exercise)" <|
             \() ->
-                Expect.equal (Term (Quantity 1.0 "JmF") (Quantity 2.0 "smF"))
-                    ((combine t2 >> combine t1)
+                Expect.equal (Term (Quantity 1.0 [ "J", "m", "F" ]) (Quantity 2.0 [ "s", "m", "F" ]))
+                    ((multiply t2 >> multiply t1)
                         t3
                     )
-        , test "Test 3-way combining with pipelines (exercise)" <|
+        , test "Test 3-way multiplication with pipelines (exercise)" <|
             \() ->
-                Expect.equal (Term (Quantity 1.0 "JmF") (Quantity 2.0 "smF"))
-                    (t3 |> combine t2 |> combine t1)
+                Expect.equal (Term (Quantity 1.0 [ "J", "m", "F" ]) (Quantity 2.0 [ "s", "m", "F" ]))
+                    (t3 |> multiply t2 |> multiply t1)
         , test "Test unit counting" <|
             \() ->
                 Expect.equal
                     (Dict.fromList
-                        [ ( 'a', 1 )
-                        , ( 'b', 2 )
-                        , ( 'c', 3 )
+                        [ ( "a", 1 )
+                        , ( "b", 2 )
+                        , ( "c", 3 )
                         ]
                     )
-                    (countUnits "abbccc")
+                    (unitListToDict [ "a", "b", "b", "c", "c", "c" ])
         , test "Test unit cancelling" <|
             \() ->
                 Expect.equal
                     (Dict.fromList
-                        [ ( 'a', 0 )
-                        , ( 'b', 1 )
-                        , ( 'c', 2 )
-                        , ( 'd', -1 )
+                        [ ( "a", 0 )
+                        , ( "b", 1 )
+                        , ( "c", 2 )
+                        , ( "d", -1 )
                         ]
                     )
-                    (cancelUnits "abbccc" "abcd")
+                    (combineUnitNumerAndDenom [ "a", "b", "b", "c", "c", "c" ] [ "a", "b", "c", "d" ])
         , test "Test unit cancelling, part deux" <|
             \() ->
                 Expect.equal
-                    ( Dict.fromList
-                        [ ( 'b', 1 )
-                        , ( 'c', 2 )
-                        ]
-                    , Dict.fromList
-                        [ ( 'd', 1 )
+                    ( [ "b"
+                      , "c"
+                      , "c"
+                      ]
+                    , [ "d"
+                      ]
+                    )
+                    (cancelUnits [ "a", "b", "b", "c", "c", "c" ] [ "a", "b", "c", "d" ])
+        , test "Test dict to list" <|
+            \() ->
+                Expect.equal
+                    (dictToList
+                        (Dict.fromList
+                            [ ( "b", 1 )
+                            , ( "c", 2 )
+                            ]
+                        )
+                    )
+                    [ "b", "c", "c" ]
+        , test "Test Term to string" <|
+            \() ->
+                Expect.equal
+                    (dictFractToDisplayString
+                        ( Dict.fromList
+                            [ ( "b", 1 )
+                            , ( "c", 2 )
+                            ]
+                        , Dict.fromList
+                            [ ( "d", 1 )
+                            ]
+                        )
+                    )
+                    ( "b∙c<sup>2</sup>", "d" )
+        , test "Test complete simple operation" <|
+            \() ->
+                Expect.equal (Term (Quantity 6.25 [ "m" ]) (Quantity 1.0 [ "" ]))
+                    (multiplyList
+                        [ Term (Quantity 10.0 [ "k" ]) (Quantity 1.0 [ "" ])
+                        , Term (Quantity 1.0 [ "m" ]) (Quantity 1.6 [ "k" ])
                         ]
                     )
-                    (cancelUnitsDeux "abbccc" "abcd")
+        , test "Test complete operation 1" <|
+            \() ->
+                Expect.equal (Term (Quantity 6.25 [ "mi" ]) (Quantity 1.0 []))
+                    (multiplyList
+                        [ Term (Quantity 10.0 [ "km" ]) (Quantity 1.0 [])
+                        , Term (Quantity 1.0 [ "mi" ]) (Quantity 1.6 [ "km" ])
+                        ]
+                    )
+        , test "Test complete operation 2" <|
+            \() ->
+                Expect.equal (Term (Quantity 11.25 [ "mi" ]) (Quantity 1.0 [ "h" ]))
+                    (multiplyList
+                        [ Term (Quantity 5.0 [ "m" ]) (Quantity 1.0 [ "s" ])
+                        , Term (Quantity 3600.0 [ "s" ]) (Quantity 1.0 [ "h" ])
+                        , Term (Quantity 1.0 [ "mi" ]) (Quantity 1600.0 [ "m" ])
+                        ]
+                    )
+        , test "Test complete operation 3" <|
+            \() ->
+                Expect.equal (Term (Quantity 1 [ "mi", "mi" ]) (Quantity 1.0 [ "h" ]))
+                    (multiplyList
+                        [ Term (Quantity 1.0 [ "m", "mi" ]) (Quantity 1.0 [ "s" ])
+                        , Term (Quantity 1.0 [ "s" ]) (Quantity 1.0 [ "h" ])
+                        , Term (Quantity 1.0 [ "mi" ]) (Quantity 1.0 [ "m" ])
+                        ]
+                    )
         ]
